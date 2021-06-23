fb9410ea888e6c9218d60f3612af441e705343f6
nqrmn <85611287+nqrmn@users.noreply.github.com>
2021-06-23 17:44:17 +0200

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,10 @@
+# simple-python-calculato
+def calculator():
+
+    x = input("number 1: ")
+    y = input("number 2: ")
+    result = int(x) + int(y)
+
+    print("result = " + str(result))
+    
+calculator()
